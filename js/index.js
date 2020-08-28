0a92b92e3d7507d2bf366c9e13bdeddea6d2d3aa
范晋清 <fanjinqing89@163.com>
2020-08-28 09:01:24 +0800

    完整pc版页面

--- js/index.js
+++ js/index.js
@@ -5,23 +5,23 @@
 	});
 
 	// 切换项目展示栏
-	$(".project-pre-list .row .col-md-2-5").click(function(event) {
+	$(".project-pre-list .row .col-sm-2-5").click(function(event) {
 		$(this).addClass("project-active").siblings().removeClass("project-active")
 		$("#pictures .row").eq($(this).index()).addClass("picture-active").siblings().removeClass("picture-active")
 	})
 })()
 // 鼠标悬停
-$(".mover .col-md-2").mouseover(e => {
+$(".mover .col-sm-2").mouseover(e => {
 	let getName = '.' + e.currentTarget.classList[1] + '-a'
-	$('.project-list .mouse .col-md-2').css('display', 'none')
+	$('.project-list .mouse .col-sm-2').css('display', 'none')
 	$(getName).css('display', 'block')
 	$(getName).css('clear', 'left')
 })
 
-$(".mover .col-md-2").mouseout(e => {
+$(".mover .col-sm-2").mouseout(e => {
 	let getName = '.' + e.currentTarget.classList[1] + '-a'
-	$('.project-list .mouse .col-md-2').css('display', 'none')
-	$('.project-list .mouse .col-md-2').css('clear', 'left')
+	$('.project-list .mouse .col-sm-2').css('display', 'none')
+	$('.project-list .mouse .col-sm-2').css('clear', 'left')
 	$(getName).mouseover((e) => {
 		let getName = '.' + e.currentTarget.classList[1]
 		$(getName).css('display', 'block')
@@ -35,18 +35,25 @@ $(".mover .col-md-2").mouseout(e => {
 
 
 //项目展示列表右侧图片宫格
-$(
-	function() {
-		$('.table tr td').click(
-			function() {
-				$('.modal-content').html($(this).html());
+$('#imgdiv img').click(e => {
+	// console.log(e.target.outerHTML)
+	$('.modal').css('display', 'block')
+	$('.modal').append(e.target.outerHTML)
+	$('.modal').click(() => {
+		$('.modal').css('display', 'none')
+	})
+})
 
-				$('#myModal').modal('toggle');
-			}
-		);
-	}
-);
-
+// $(
+// 	function() {
+// 		$('#imgdiv img').click(
+// 			function() {
+// 				$('.modal').html($(this).html());
+// 				$('.modal').modal('toggle');
+// 			}
+// 		);
+// 	}
+// );
 // swiper
 var swiper = new Swiper('.swiper-container', {
 	slidesPerView: 3, //显示3个
